279126419b42dbc5c1943f19b4bb432894b99515
shivam-attree <33188692+shivam-attree@users.noreply.github.com>
2018-03-18 14:18:02 +0530

    Add files via upload

---
+++ HuskyBot/credentials.py
@@ -0,0 +1,5 @@
+# Create variables for each key, secret, token
+consumer_key = 'XXXXXXXXXXXXXXXXXXXX'
+consumer_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
